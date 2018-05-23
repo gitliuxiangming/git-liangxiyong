88a27292e51c5fc3e81910d5efb3b4cc9009f5cc
刘向明 <719327306@qq.com>
2018-05-23 20:58:05 +0800

    js进阶

--- JavaScript/JS/动画定时器.js
+++ JavaScript/JS/动画定时器.js
@@ -7,55 +7,55 @@ Ex：animation(obj,{width:100,height；100}，true,function(){
 })
 */
 function animation(obj,opation,sudu,fn){
-				clearInterval(obj.time);
-				var speed=0;
-				var stop=false;				
-				obj.time = setInterval(function(){
-					var stopall=true;
-					for(attr in opation){
-						var dqz=parseFloat(getComputedStyle(obj,true)[attr]);
-						if(attr == 'opacity'){
-							dqz=dqz*100;
-						}
-						if (sudu) {
-							if(dqz<=opation[attr]){
-								speed= 10;
-							}else{
-								speed= -10;
-							}
-							if (Math.abs(opation[attr]-dqz)<Math.abs(speed)) {
-								stop=true;								
-							}else{
-								stopall=false;
-							}
-						}else{
-							speed=(opation[attr]-dqz)/20;
-							speed=speed>0 ? Math.ceil(speed) : Math.floor(speed);
-							if (!speed) {
-								stop=true;
-							}else{
-								stopall=false;
-							}
-						}					
-						if(stop){							
-							if(attr == 'opacity'){
-								obj.style[attr]=opation[attr]/100;
-							}else{
-								obj.style[attr] = opation[attr]+'px';
-							}
-						}else{
-							if(attr == 'opacity'){
-								obj.style[attr]=(dqz+speed)/100;
-							}else{
-								obj.style[attr] = dqz+speed+'px';
-							}
-						}
-					}
-					if (stopall) {
-						clearInterval(obj.time);
-						if(fn){
-							fn();
-						}
-					}
-				},30)
+	clearInterval(obj.time);
+	var speed=0;
+	var stop=false;				
+	obj.time = setInterval(function(){
+		var stopall=true;
+		for(attr in opation){
+			var dqz=parseFloat(getComputedStyle(obj,true)[attr]);
+			if(attr == 'opacity'){
+				dqz=dqz*100;
 			}
+			if (sudu) {
+				if(dqz<=opation[attr]){
+					speed= 10;
+				}else{
+					speed= -10;
+				}
+				if (Math.abs(opation[attr]-dqz)<Math.abs(speed)) {
+					stop=true;								
+				}else{
+					stopall=false;
+				}
+			}else{
+				speed=(opation[attr]-dqz)/20;
+				speed=speed>0 ? Math.ceil(speed) : Math.floor(speed);
+				if (!speed) {
+					stop=true;
+				}else{
+					stopall=false;
+				}
+			}					
+			if(stop){							
+				if(attr == 'opacity'){
+					obj.style[attr]=opation[attr]/100;
+				}else{
+					obj.style[attr] = opation[attr]+'px';
+				}
+			}else{
+				if(attr == 'opacity'){
+					obj.style[attr]=(dqz+speed)/100;
+				}else{
+					obj.style[attr] = dqz+speed+'px';
+				}
+			}
+		}
+		if (stopall) {
+			clearInterval(obj.time);
+			if(fn){
+				fn();
+			}
+		}
+	},30)
+}
