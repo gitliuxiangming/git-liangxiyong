c9459d379cf7e03e11f4a1da838019b17549d90f
刘向明 <719327306@qq.com>
2018-07-23 18:00:24 +0800

    add my baba

--- NodeJs/007-stream/004-readlike.js
+++ NodeJs/007-stream/004-readlike.js
@@ -13,12 +13,11 @@ class Likereadable extends EventEmitter{
 			}
 		})
 	}
+
 	_dispath(){
 		let startLength = this.data.length;
 		let endLength = startLength;
 		while(endLength > 0){
-			/*let start = startLength - endLength;
-			endLength -= startLength - offsetLength;*/
 			let start = startLength - endLength;
 			let end = start + this.offsetLength;
 			let tmp = this.data.slice(start,end);
@@ -26,6 +25,7 @@ class Likereadable extends EventEmitter{
 			this.emit('data',buf);
 			endLength -= this.offsetLength;
 		}
+		this.emit('end');
 	}
 }
 
@@ -34,10 +34,10 @@ let data='aaaaabbbbbcccccddddd';
 
 const reads = new Likereadable(data,5);
 
-// const reads = getReadableStreamSomehow();
 reads.on('data', (chunk) => {
   console.log(chunk.toString());
 });
+
 reads.on('end', () => {
   console.log('There will be no more data.');
 });
